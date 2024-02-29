ea089bd4404c25aa18edd7cedfc2028c0bc03587
0vitor <w.wutaes@gmail.com>
2024-02-29 15:22:43 -0300

    chore: install pg

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,13 @@
 import { createServer } from "node:http";
+import { Client } from "pg";
+
+const client = new Client({
+  user: "postgres",
+  password: "postgres",
+  host: "database-1.chftne5ljznd.us-east-1.rds.amazonaws.com",
+  port: 5432,
+  database: "database-1",
+});
 
 const server = createServer((request, response) => {
   response.write("Hello World");
@@ -6,4 +15,6 @@ const server = createServer((request, response) => {
   return response.end();
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("rodando...");
+});
