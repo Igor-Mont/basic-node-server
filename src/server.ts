e58f477e3fc3f35b463b30f51d54aaf4c0768bc3
Igor-Mont <igornathandev@gmail.com>
2024-03-04 17:20:59 -0500

    fix: remove logs

--- src/server.ts
+++ src/server.ts
@@ -9,40 +9,50 @@ const client = new Client({
   database: "atividade-3",
 });
 
-client.connect()
+client
+  .connect()
   .then(() => console.log("Connected with database 📦..."))
-  .catch(err => console.error('Error connecting to database', err));
+  .catch((err) => console.error("Error connecting to database", err));
 
-const server = createServer(async (request: IncomingMessage, response: ServerResponse) => {
-  if (request.method === "GET") {
-    console.log("GET");
+const server = createServer(
+  async (request: IncomingMessage, response: ServerResponse) => {
+    if (request.method === "GET") {
+      console.log("GET");
 
-    try {
-      const { rows } = await client.query("SELECT * FROM bilheteriadigital.administradores");
-      console.log("data");
-      console.log(rows);
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.end(JSON.stringify(rows));
-    } catch (error) {
-      console.error(error);
-      response.writeHead(500, { "Content-Type": "text/plain" });
-      response.end("Internal Server Error");
-    }
-  }
-
-  if (request.method === "POST") {
-    console.log("POST");
-
-    let body = "";
-    request.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    request.on("end", async () => {
       try {
-        const data = JSON.parse(body);
-        const { id, email, telefone, primeiro_nome, segundo_nome, cpf, data_de_nascimento } = data;
-        const insertQuery = `INSERT INTO bilheteriadigital.administradores (
+        const { rows } = await client.query(
+          "SELECT * FROM bilheteriadigital.administradores"
+        );
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(rows));
+      } catch (error) {
+        console.error(error);
+        response.writeHead(500, { "Content-Type": "text/plain" });
+        response.end("Internal Server Error");
+      }
+    }
+
+    if (request.method === "POST") {
+      console.log("POST");
+
+      let body = "";
+      request.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      request.on("end", async () => {
+        try {
+          const data = JSON.parse(body);
+          const {
+            id,
+            email,
+            telefone,
+            primeiro_nome,
+            segundo_nome,
+            cpf,
+            data_de_nascimento,
+          } = data;
+          const insertQuery = `INSERT INTO bilheteriadigital.administradores (
             id, 
             email, 
             telefone, 
@@ -58,28 +68,28 @@ const server = createServer(async (request: IncomingMessage, response: ServerRes
             $5, 
             $6, 
             $7)`;
-        const values = [
-          id, 
-          email, 
-          telefone, 
-          primeiro_nome, 
-          segundo_nome, 
-          cpf, 
-          data_de_nascimento
-        ];
+          const values = [
+            id,
+            email,
+            telefone,
+            primeiro_nome,
+            segundo_nome,
+            cpf,
+            data_de_nascimento,
+          ];
 
-        await client.query(insertQuery, values);
-        console.log(data);
-        response.writeHead(200, { "Content-Type": "application/json" });
-        response.end("Data inserted successfully");
-      } catch (error) {
-        console.error(error);
-        response.writeHead(500, { "Content-Type": "text/plain" });
-        response.end("Internal Server Error");
-      }
-    });
+          await client.query(insertQuery, values);
+          response.writeHead(200, { "Content-Type": "application/json" });
+          response.end(JSON.stringify(data));
+        } catch (error) {
+          console.error(error);
+          response.writeHead(500, { "Content-Type": "text/plain" });
+          response.end("Internal Server Error");
+        }
+      });
+    }
   }
-});
+);
 
 server.listen(3000, () => {
   console.log("Server listen on port 3000 🔥...");
