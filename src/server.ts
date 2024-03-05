9e53c6ed77ab9b0991072ed347076950a10d3137
0vitor <w.wutaes@gmail.com>
2024-03-04 21:28:48 -0300

    refactor: remove port

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,6 @@ const client = new Client({
   user: process.env.DB_USER,
   password: process.env.PASSWORD,
   host: process.env.HOST,
-  port: process.env.PORT,
   database: process.env.DATABASE,
 });
 
